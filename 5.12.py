c11f1055526493aa783eb674982cdbd765ef0f35
negativex <negativex228@gmail.com>
2019-11-21 10:00:46 +0200

    lab 5 (edit)

--- 5.12.py
+++ 5.12.py
@@ -3,9 +3,9 @@ import sympy.plotting as splot
 
 u, v = sp.symbols('u v')
 
-splot.plot3d_parametric_surface(4 + (3 + sp.cos(v)) * sp.sin(u), 4 + (3 + sp.cos(v)) * sp.cos(u), 4 + sp.sin(v),
-                                (u, -0, 2 * sp.pi),
-                                (v, -0, 2 * sp.pi))
-splot.plot3d_parametric_surface(8 + (3 + sp.cos(v)) * sp.cos(u), 3 + sp.sin(v), 4 + (3 + sp.cos(v)) * sp.sin(u),
-                                (u, -0, 2 * sp.pi),
-                                (v, -0, 2 * sp.pi))
+splot.plot3d_parametric_surface((4 + (3 + sp.cos(v)) * sp.sin(u), 4 + (3 + sp.cos(v)) * sp.cos(u), 4 + sp.sin(v),
+                                 (u, 0, 2 * sp.pi),
+                                 (v, 0, 2 * sp.pi)),
+                                (8 + (3 + sp.cos(v)) * sp.cos(u), 3 + sp.sin(v), 4 + (3 + sp.cos(v)) * sp.sin(u),
+                                 (u, 0, 2 * sp.pi),
+                                 (v, 0, 2 * sp.pi)))
